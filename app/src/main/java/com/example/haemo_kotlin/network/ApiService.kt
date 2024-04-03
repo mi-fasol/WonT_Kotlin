667a1da4bac660f9c74e4c2612da45627961a074
mi-fasol <minyoong39@naver.com>
2024-04-04 00:06:57 +0900

    feat: club page API 추가

--- app/src/main/java/com/example/haemo_kotlin/network/ApiService.kt
+++ app/src/main/java/com/example/haemo_kotlin/network/ApiService.kt
@@ -1,13 +1,13 @@
 package com.example.haemo_kotlin.network
 
-import com.example.haemo_kotlin.model.LoginModel
-import com.example.haemo_kotlin.model.PostModel
-import com.example.haemo_kotlin.model.UserModel
-import com.example.haemo_kotlin.model.UserResponseModel
+import com.example.haemo_kotlin.model.post.ClubPostModel
+import com.example.haemo_kotlin.model.user.LoginModel
+import com.example.haemo_kotlin.model.post.PostModel
+import com.example.haemo_kotlin.model.user.UserModel
+import com.example.haemo_kotlin.model.user.UserResponseModel
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -26,4 +26,10 @@ interface ApiService {
 
     @POST("/user")
     suspend fun registerUser(@Body user: UserModel) : Response<UserResponseModel>
+
+    @GET("club")
+    suspend fun getClubPost(): Response<List<ClubPostModel>>
+
+    @GET("club/{id}")
+    suspend fun getOneClubPost(@Path("id") idx: Int): Response<ClubPostModel>
 }
