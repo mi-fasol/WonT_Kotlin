53cc58aaba05ecd8e5d9a0057ac245b551d257bc
mi-fasol <minyoong39@naver.com>
2024-04-04 20:56:16 +0900

    feat: HotPlaceScreen 추가

---
+++ app/src/main/java/com/example/haemo_kotlin/screen/main/HotPlaceScreen.kt
@@ -0,0 +1,271 @@
+package com.example.haemo_kotlin.screen.main
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.haemo_kotlin.R
+import com.example.haemo_kotlin.model.post.HotPlacePostModel
+import com.example.haemo_kotlin.model.post.PostModel
+import com.example.haemo_kotlin.util.MainBottomNavigation
+import com.example.haemo_kotlin.util.MainPageAppBar
+import com.example.haemo_kotlin.viewModel.HotPlacePostViewModel
+import com.example.haemo_kotlin.viewModel.PostViewModel
+
+@SuppressLint("UnusedBoxWithConstraintsScope")
+@Composable
+fun HotPlaceScreen(postViewModel: HotPlacePostViewModel, navController: NavController) {
+    val postList = postViewModel.hotPlacePostList.collectAsState().value
+    val popularPostList = postViewModel.popularHotPlace.collectAsState().value
+
+    LaunchedEffect(postList) {
+        postViewModel.getHotPlacePost()
+    }
+    LaunchedEffect(popularPostList) {
+        postViewModel.getPopularHotPlace()
+    }
+
+    Scaffold(
+        topBar = {
+            MainPageAppBar("요즘 핫한 핫플레이스", navController)
+        },
+        bottomBar = { MainBottomNavigation(navController = navController) },
+    ) { innerPadding ->
+        BoxWithConstraints {
+            Column(
+                modifier = Modifier
+                    .padding(
+                        bottom = innerPadding.calculateBottomPadding() + 10.dp
+                    )
+            ) {
+                Divider(thickness = 0.5.dp, color = Color(0xffbbbbbb))
+                Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+                    PopularPlace(popularPostList, postViewModel)
+                    HotPlaceBoard(postList = postList, viewModel = postViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PopularPlace(postList: List<HotPlacePostModel>, viewModel: HotPlacePostViewModel) {
+    when (postList.size) {
+        0 -> {
+            Box {}
+        }
+
+        else -> {
+            Column(
+                Modifier.padding(top = 15.dp, bottom = 10.dp)
+            ) {
+                Text(
+                    "현재, 가장 인기있는 핫플",
+                    fontSize = 15.sp,
+                    color = Color(0xff414141),
+                    fontWeight = FontWeight.SemiBold
+                )
+                LazyRow(
+                ) {
+                    items(postList.size) { idx ->
+                        PopularPlaceItem(postList[idx], viewModel)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PopularPlaceItem(post: HotPlacePostModel, viewModel: HotPlacePostViewModel) {
+    val config = LocalConfiguration.current
+    val screenWidth = config.screenWidthDp
+    val screenHeight = config.screenHeightDp
+    Row {
+        Box(
+            modifier = Modifier
+                .height((screenHeight / 3.4).dp)
+                .padding(top = 5.dp, bottom = 15.dp)
+                .width((screenWidth / 2).dp)
+                .background(Color.Unspecified, RoundedCornerShape(15.dp))
+                .graphicsLayer(alpha = 0.8f),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Unspecified, RoundedCornerShape(15.dp)),
+                contentAlignment = Alignment.Center,
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(15.dp)),
+                    elevation = 0.dp,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.dummy_image),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Unspecified, RoundedCornerShape(15.dp))
+                    )
+                }
+                Column(
+                    Modifier
+                        .padding(13.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Icon(
+                        Icons.Default.Favorite,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier
+                            .size((screenWidth / 20).dp)
+                            .fillMaxWidth()
+                            .align(Alignment.End)
+                    )
+                    Column(
+                    ) {
+                        Text(
+                            post.title,
+                            fontSize = 18.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            post.content,
+                            color = Color.White,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+    }
+}
+
+@Composable
+fun HotPlaceBoard(postList: List<HotPlacePostModel>, viewModel: HotPlacePostViewModel) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        verticalItemSpacing = 5.dp,
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.fillMaxWidth(),
+        content = {
+            items(postList.size) { idx ->
+                HotPlaceBoardItem(postList[idx], viewModel)
+            }
+        }
+    )
+}
+
+@Composable
+fun HotPlaceBoardItem(post: HotPlacePostModel, viewModel: HotPlacePostViewModel) {
+    val config = LocalConfiguration.current
+    val screenWidth = config.screenWidthDp
+    val screenHeight = config.screenHeightDp
+    Box(
+        modifier = Modifier
+            .height((screenHeight / 5.5).dp)
+            .padding(top = 5.dp, bottom = 15.dp)
+            .width((screenWidth / 3.5).dp)
+            .background(Color.Unspecified, RoundedCornerShape(15.dp))
+            .graphicsLayer(alpha = 0.8f),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Unspecified, RoundedCornerShape(15.dp)),
+            contentAlignment = Alignment.Center,
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(15.dp)),
+                elevation = 0.dp,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dummy_image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Unspecified, RoundedCornerShape(15.dp))
+                )
+            }
+            Column(
+                Modifier
+                    .padding(13.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Icon(
+                    Icons.Default.Favorite,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size((screenWidth / 20).dp)
+                        .fillMaxWidth()
+                        .align(Alignment.End)
+                )
+                Text(
+                    post.title,
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/haemo_kotlin/viewModel/HotPlacePostViewModel.kt
@@ -0,0 +1,116 @@
+package com.example.haemo_kotlin.viewModel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.haemo_kotlin.model.HangulUtils
+import com.example.haemo_kotlin.model.post.ClubPostModel
+import com.example.haemo_kotlin.model.post.HotPlacePostModel
+import com.example.haemo_kotlin.model.post.containsHangulSearch
+import com.example.haemo_kotlin.network.Resource
+import com.example.haemo_kotlin.repository.PostRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlin.collections.joinToString
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HotPlacePostViewModel @Inject constructor(
+    private val repository: PostRepository
+) : ViewModel() {
+
+    private val _hotPlacePostList = MutableStateFlow<List<HotPlacePostModel>>(emptyList())
+    val hotPlacePostList: StateFlow<List<HotPlacePostModel>> = _hotPlacePostList.asStateFlow()
+
+    private val _popularHotPlace = MutableStateFlow<List<HotPlacePostModel>>(emptyList())
+    val popularHotPlace: StateFlow<List<HotPlacePostModel>> = _popularHotPlace.asStateFlow()
+
+
+    private val _hotPlaceModel = MutableStateFlow<HotPlacePostModel?>(null)
+    val hotPlaceModel: StateFlow<HotPlacePostModel?> = _hotPlaceModel.asStateFlow()
+
+    private val _hotPlacePostState = MutableStateFlow<Resource<HotPlacePostModel>>(Resource.loading(null))
+    val hotPlacePostState: StateFlow<Resource<HotPlacePostModel>> = _hotPlacePostState.asStateFlow()
+
+    private val _hotPlacePostListState =
+        MutableStateFlow<Resource<List<HotPlacePostModel>>>(Resource.loading(null))
+    val hotPlacePostListState: StateFlow<Resource<List<HotPlacePostModel>>> =
+        _hotPlacePostListState.asStateFlow()
+
+    private val _popularHotPlaceListState =
+        MutableStateFlow<Resource<List<HotPlacePostModel>>>(Resource.loading(null))
+    val popularHotPlaceListState: StateFlow<Resource<List<HotPlacePostModel>>> =
+        _popularHotPlaceListState.asStateFlow()
+
+    suspend fun getHotPlacePost() {
+        viewModelScope.launch {
+            _hotPlacePostListState.value = Resource.loading(null)
+            try {
+                val response = repository.getHotPlacePost()
+                if (response.isSuccessful && response.body() != null) {
+                    val postList = response.body()
+                    _hotPlacePostList.value = postList!!
+                    _hotPlacePostListState.value = Resource.success(postList)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                    _hotPlacePostListState.value = Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _hotPlacePostListState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    suspend fun getOneHotPlacePost(idx: Int) {
+        viewModelScope.launch {
+            _hotPlacePostState.value = Resource.loading(null)
+            try {
+                val response = repository.getHotPlaceById(idx)
+                if (response.isSuccessful && response.body() != null) {
+                    val post = response.body()
+                    _hotPlaceModel.value = post!!
+                    _hotPlacePostState.value = Resource.success(post)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 하나 에러 응답: $errorBody")
+                    _hotPlacePostState.value = Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _hotPlacePostState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    suspend fun getPopularHotPlace() {
+        viewModelScope.launch {
+            _hotPlacePostListState.value = Resource.loading(null)
+            try {
+                val response = repository.getPopularHotPlace()
+                if (response.isSuccessful && response.body() != null) {
+                    val postList = response.body()
+                    _popularHotPlace.value = postList!!
+                    _hotPlacePostListState.value = Resource.success(postList)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                    _hotPlacePostListState.value = Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _hotPlacePostListState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+}
