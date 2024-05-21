9f31acb48dd89efdcb90bba9d757187b9bff9803
mi-fasol <minyoong39@naver.com>
2024-05-22 00:47:39 +0900

    feat: 회원 탈퇴 페이지 기능 완성

--- app/src/main/java/com/example/haemo_kotlin/screen/setting/WithdrawScreen.kt
+++ app/src/main/java/com/example/haemo_kotlin/screen/setting/WithdrawScreen.kt
@@ -2,7 +2,6 @@ package com.example.haemo_kotlin.screen.setting
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,14 +12,13 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarHost
+import androidx.compose.material.Checkbox
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -28,10 +26,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -39,10 +41,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.haemo_kotlin.MainActivity
 import com.example.haemo_kotlin.R
 import com.example.haemo_kotlin.StartActivity
-import com.example.haemo_kotlin.screen.intro.UserRegisterButton
 import com.example.haemo_kotlin.util.SettingScreenAppBar
 import com.example.haemo_kotlin.util.SharedPreferenceUtil
 import com.example.haemo_kotlin.viewModel.MainViewModel
@@ -66,9 +66,8 @@ fun WithdrawScreen(
             Column(
                 modifier = Modifier
                     .padding(
-                        top = innerPadding.calculateTopPadding(),
-                        bottom = innerPadding.calculateBottomPadding()
-                    )
+                        top = innerPadding.calculateTopPadding()
+                    ).fillMaxSize().background(Color.White)
             ) {
                 WithdrawInfo(mainColor = mainColor, userViewModel)
             }
@@ -78,10 +77,13 @@ fun WithdrawScreen(
 
 @Composable
 fun WithdrawInfo(mainColor: Int, userViewModel: UserViewModel) {
+    val isChecked = remember { mutableStateOf(false) }
     val context = LocalContext.current
     val nickname = SharedPreferenceUtil(context).getUser().nickname
     Box(
-        modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp)
+        modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 30.dp)
+            .background(Color.White)
     ) {
         Column {
             Text(
@@ -92,17 +94,28 @@ fun WithdrawInfo(mainColor: Int, userViewModel: UserViewModel) {
                 modifier = Modifier.padding(bottom = 20.dp)
             )
             WithdrawCheckNotification(mainColor)
-            Row(){
+            Row(modifier= Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = isChecked.value, onCheckedChange = {
+                    isChecked.value = !isChecked.value
+                })
+                Text(
+                    "안내사항을 모두 확인했습니다.",
+                    fontSize = 14.sp,
+                    color = colorResource(id = R.color.settingScreenContentTextColor)
+                )
             }
-            WithdrawButton(userViewModel, mainColor)
+            WithdrawButton(userViewModel, mainColor, isChecked)
         }
     }
 }
 
 @Composable
 fun WithdrawCheckNotification(mainColor: Int) {
-    Box(modifier = Modifier.padding(10.dp)) {
-        Column(verticalArrangement = Arrangement.SpaceBetween) {
+    Box(
+        modifier = Modifier
+            .background(colorResource(id = mainColor).copy(alpha = 0.07f))
+    ) {
+        Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.padding(13.dp).fillMaxHeight(0.17f)) {
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
@@ -116,7 +129,7 @@ fun WithdrawCheckNotification(mainColor: Int) {
                 )
                 Text(
                     "지금 탈퇴하시면 서비스 악용 방지를 위해 재가입이 3일 간 제한됩니다.",
-                    fontSize = 12.sp,
+                    fontSize = 14.sp,
                     color = colorResource(id = R.color.settingScreenContentTextColor)
                 )
             }
@@ -133,7 +146,7 @@ fun WithdrawCheckNotification(mainColor: Int) {
                 )
                 Text(
                     "프로필, 작성글 등 모든 개인 정보가 삭제됩니다.",
-                    fontSize = 12.sp,
+                    fontSize = 14.sp,
                     color = colorResource(id = R.color.settingScreenContentTextColor)
                 )
             }
@@ -143,7 +156,7 @@ fun WithdrawCheckNotification(mainColor: Int) {
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun WithdrawButton(viewModel: UserViewModel, mainColor: Int) {
+fun WithdrawButton(viewModel: UserViewModel, mainColor: Int, isChecked: MutableState<Boolean>) {
     val context = LocalContext.current
     val launcher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
@@ -161,7 +174,7 @@ fun WithdrawButton(viewModel: UserViewModel, mainColor: Int) {
         onClick = {
             viewModel.deleteUser()
         },
-        enabled = true,
+        enabled = isChecked.value,
         colors = ButtonDefaults.buttonColors(
             containerColor = colorResource(id = mainColor),
             contentColor = Color.White,
@@ -171,7 +184,7 @@ fun WithdrawButton(viewModel: UserViewModel, mainColor: Int) {
         shape = RoundedCornerShape(15.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.2f)
+
     ) {
         Text("회원 탈퇴", color = Color.White, fontWeight = FontWeight.SemiBold)
     }
